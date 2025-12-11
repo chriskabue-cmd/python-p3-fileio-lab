e63cbb7b5afbcf66d2b0f2c45594f4693de87c9f
Chris kabue <chris.kabue@student.moringaschool.com>
2025-12-11 21:28:32 +0300

    python-p3-fileio-lab

---
+++ .github/lib/file_io.py
@@ -0,0 +1,16 @@
+def write_file(file_name, file_content):
+    file_path = f"{str(file_name)}.txt"
+    with open(file_path, "w") as file:
+        file.write(file_content)
+
+
+def append_file(file_name, append_content):
+    file_path = f"{str(file_name)}.txt"
+    with open(file_path, "a") as file:
+        file.write(append_content)
+
+
+def read_file(file_name):
+    file_path = f"{str(file_name)}.txt"
+    with open(file_path, "r") as file:
+        return file.read()
--- lib/file_io.py
+++
@@ -1,8 +0,0 @@
-def write_file(file_name, file_content):
-    pass
-
-def append_file(file_name, append_content):
-    pass
-
-def read_file(file_name):
-    pass
